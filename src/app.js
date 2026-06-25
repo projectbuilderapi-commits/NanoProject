dc54825f0db8bbf3eb56f22afab04ab901276627
shasi kumar <shasikumar@varnarc.com>
2026-06-25 15:35:47 +0530

    New One

--- src/app.js
+++ src/app.js
@@ -20,7 +20,7 @@ app.use(
   cors({
     origin: [
       "http://localhost:5173",
-      "https://projectbuilderapi.netlify.app/"
+      "https://projectbuilderapi.netlify.app"
     ],
     credentials: true,
   })
